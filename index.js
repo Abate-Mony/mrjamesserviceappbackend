7bfe1b097a68ee8da6326695f69dd8e6f5c39441
mmanuel <bateemma14@gmail.com>
2023-04-27 08:42:09 +0100

    user

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors())
-    // const { NOTFOUND, ERROR, AdminAuth } = require("./middlewares")
+const { NOTFOUND, ERROR, AdminAuth } = require("./middlewares")
     // const Admin = require("./models/Admin")
 
 // const { Application, User, Message, Admin: admin, Service } = require("./routes")
