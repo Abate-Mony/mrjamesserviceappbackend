18dda35ce1b3a53c4741d58aad77786265a4df51
mmanuel <bateemma14@gmail.com>
2023-04-27 15:38:27 +0100

    dont give up

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors())
 const { NOTFOUND, ERROR } = require("./middlewares")
-const Admin = require("./models/Admin")
+    // const Admin = require("./models/Admin")
 
 // const { Application, User, Message, Admin: admin, Service } = require("./routes")
 const Application = require("./routes/Application")
@@ -33,14 +33,14 @@ module.exports = app
 const start = async() => {
     try {
         app.listen(port, function() {
-            console.log(`app is running on port ${port }`)
-        })
-        require("./db/connections");
-        await Admin.deleteMany({});
-        await Admin.create({
-            phone: 672301714,
-            password: "mrjames"
-        })
+                console.log(`app is running on port ${port }`)
+            })
+            // require("./db/connections");
+            // await Admin.deleteMany({});
+            // await Admin.create({
+            //     phone: 672301714,
+            //     password: "mrjames"
+            // })
     } catch (err) {
         console.log(err)
     }
